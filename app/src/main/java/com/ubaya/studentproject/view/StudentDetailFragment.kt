de93fef4709150d46827a3668439db4fac2c7ba9
latansa123 <latansatahsya@gmail.com>
2024-09-26 13:43:34 +0700

    Detail student

--- app/src/main/java/com/ubaya/studentproject/view/StudentDetailFragment.kt
+++ app/src/main/java/com/ubaya/studentproject/view/StudentDetailFragment.kt
@@ -5,7 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.ubaya.studentproject.R
+import com.ubaya.studentproject.databinding.FragmentStudentDetailBinding
+import com.ubaya.studentproject.databinding.FragmentStudentListBinding
+import com.ubaya.studentproject.viewmodel.DetailViewModel
+import com.ubaya.studentproject.viewmodel.ListViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,43 +24,33 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class StudentDetailFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private  lateinit var binding: FragmentStudentDetailBinding
+    private lateinit var viewModel: DetailViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = FragmentStudentDetailBinding.inflate(layoutInflater,container,false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_student_detail, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudentDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StudentDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+
+        observeViewModel()
     }
+
+    fun observeViewModel() {
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            binding.txtName.setText(it.name)
+            binding.txtBod.setText(it.bod)
+            binding.txtID.setText(it.id)
+            binding.txtPhone.setText(it.phone)
+        })
+
+    }
+
 }
